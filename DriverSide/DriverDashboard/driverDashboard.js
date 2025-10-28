329904287e21e76f852d6d2ad54e56a0f10d009b
DevNiNi18 <richardchidimma433@gmail.com>
2025-10-28 13:42:25 +0100

    worked major functionalities

--- DriverSide/DriverDashboard/driverDashboard.js
+++ DriverSide/DriverDashboard/driverDashboard.js
@@ -1,6 +1,9 @@
 const toggle = document.getElementById('toggle_switch');
 const offCircle = document.querySelector('.off-circle');
 const onCircle = document.querySelector('.on-circle');
+const notisBell = document.getElementById('notis');
+const overlay = document.getElementById('modal-overlay');
+const notisCard = document.getElementById('ride-request-card-div');
 
 
 // For the toogle switch
@@ -15,3 +18,24 @@ toggle.addEventListener('click', function () {
     offCircle.classList.remove('active');
   }
 }); 
+
+// for the notification popup; opening
+function openPopup() {
+  overlay.classList.add('active');
+  notisCard.classList.add('active');
+};
+notisBell.addEventListener('click', openPopup);
+
+// for closing popup
+function closePopup() {
+  overlay.classList.remove('active');
+  notisCard.classList.remove('active');
+};
+
+// notisBell.addEventListener('click', closePopup);
+
+overlay.addEventListener("click", function (event) {
+    if (event.target === overlay) {
+      closePopup();
+    }
+  });
