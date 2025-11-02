c5e365905a4441161ac19b2fe6da451cd003f86f
lemohajoshua <lemohajoshua89@gmail.com>
2025-11-02 17:05:42 +0100

    worked on the location and book ried page

--- PassengerSide/BookingPage/bookRide.js
+++ PassengerSide/BookingPage/bookRide.js
@@ -10,10 +10,13 @@ document.addEventListener('DOMContentLoaded', function() {
 
         const soloPrice = parseInt(rideData.soloPrice);
         const sharedPrice = parseInt(rideData.sharedPrice);
+        const destiName = rideData.destiName;
         const savedAmount = soloPrice - sharedPrice;
 
         document.getElementById('solo-price').textContent = formatPrice(soloPrice);
         document.getElementById('shared-price').textContent = formatPrice(sharedPrice);
+        document.getElementById('solo-desti-name').textContent = destiName;
+        document.getElementById('shared-desti-name').textContent =destiName;
         document.getElementById('comparison-solo').textContent = formatPrice(soloPrice);
         document.getElementById('comparison-shared').textContent = formatPrice(sharedPrice);
         document.getElementById('saved-amount').textContent = formatPrice(savedAmount);
@@ -52,12 +55,12 @@ function selectRideType(rideType) {
 
     if (rideType === 'solo') {
         soloElement.style.borderColor = '#7cd04f';
-        // soloElement.style.backgroundColor = '#f0f9eb';
+        soloElement.style.backgroundColor = '#f0f9eb';
         sharedElement.style.borderColor = '#071a39';
         sharedElement.style.backgroundColor = 'white';
     } else {
         sharedElement.style.borderColor = '#7cd04f';
-        // sharedElement.style.backgroundColor = '#f0f9eb';
+        sharedElement.style.backgroundColor = '#f0f9eb';
         soloElement.style.borderColor = '#071a39';
         soloElement.style.backgroundColor = 'white';
     }
